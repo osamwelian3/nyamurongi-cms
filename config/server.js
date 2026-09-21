81e1af9ab3d136fedb40c19224a3ebc2530b1963
Samuel Ian <osamwelian3@gmail.com>
2026-09-21 17:51:50 +0300

    add url server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://74.50.87.101:1338',
   app: {
     keys: env.array('APP_KEYS'),
   },
